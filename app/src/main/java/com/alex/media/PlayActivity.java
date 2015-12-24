7568e8428f82a2f323697db739708545d5dfd642
luojin <luojin2@mail2.sysu.edu.cn>
2015-12-24 10:46:55 +0800

    delete unused

--- app/src/main/java/com/alex/media/PlayActivity.java
+++ app/src/main/java/com/alex/media/PlayActivity.java
@@ -93,105 +93,7 @@ public class PlayActivity extends Activity implements MediaPlayer.OnCompletionLi
 				}
 			}
 		});
-		
-		
-		/*stopBtn = (Button)findViewById(R.id.stopBtn);//停止播放
-		stopBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				stop();
-			}
-		});
-		*/
-		
-		
-		
-		/*上一首、下一首*/
-//		latestBtn = (ImageButton)findViewById(R.id.latestBtn);
-//		latestBtn.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				int num = _ids.length;
-//				if(position==0){
-//					position=num-1;
-//				}else{
-//					position-=1;
-//				}
-//				System.out.println(position);
-//				int pos = _ids[position];
-//
-//				lrc_map.clear();
-//				setup();
-//				play();
-//
-//
-//			}
-//		});
-//
-//		nextButton = (ImageButton)findViewById(R.id.nextBtn);
-//		nextButton.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				int num = _ids.length;
-//				if (position==num-1){
-//					position=0;
-//				}else{
-//					position+=1;
-//				}
-//				int pos = _ids[position];
-//				lrc_map.clear();
-//				setup();
-//				play();
-//			}
-//		});
-//
-//		/*快进、快退*/
-//		forwardBtn = (ImageButton)findViewById(R.id.forwardBtn);//快进
-//		forwardBtn.setOnTouchListener(new OnTouchListener() {
-//
-//			@Override
-//			public boolean onTouch(View v, MotionEvent event) {
-//				switch (event.getAction()) {
-//				case MotionEvent.ACTION_DOWN:
-//					fHandler.post(forward);
-//					mp.pause();
-//					break;
-//
-//				case MotionEvent.ACTION_UP:
-//					fHandler.removeCallbacks(forward);
-//					mp.start();
-//					playBtn.setBackgroundResource(R.drawable.pause_selecor);
-//					break;
-//				}
-//				return false;
-//			}
-//		});
-//
-//		rewindBtn = (ImageButton)findViewById(R.id.rewindBtn);
-//		rewindBtn.setOnTouchListener(new OnTouchListener() {
-//
-//			@Override
-//			public boolean onTouch(View v, MotionEvent event) {
-//				switch (event.getAction()) {
-//				case MotionEvent.ACTION_DOWN:
-//					fHandler.post(rewind);
-//					mp.pause();
-//					break;
-//
-//				case MotionEvent.ACTION_UP:
-//					fHandler.removeCallbacks(rewind);
-//					mp.start();
-//					playBtn.setBackgroundResource(R.drawable.pause_selecor);
-//					break;
-//				}
-//				return false;
-//			}
-//		});
-		
-		
+
 		/*SeekBar进度条*/
 		seekbar = (SeekBar)findViewById(R.id.seekbar);
 		
@@ -215,34 +117,7 @@ public class PlayActivity extends Activity implements MediaPlayer.OnCompletionLi
 				}
 			}
 		});
-		
-		/*音量控制条*/
-//		soundBar = (SeekBar)findViewById(R.id.sound);
-//		soundBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-//
-//			@Override
-//			public void onStopTrackingTouch(SeekBar seekBar) {
-//				// TODO Auto-generated method stub
-//
-//			}
-//
-//			@Override
-//			public void onStartTrackingTouch(SeekBar seekBar) {
-//				// TODO Auto-generated method stub
-//
-//			}
-//
-//			@Override
-//			public void onProgressChanged(SeekBar seekBar, int progress,
-//					boolean fromUser) {
-//				// TODO Auto-generated method stub
-//				if (fromUser){
-//					int ScurrentPosition = soundBar.getProgress();
-//					mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, ScurrentPosition, 0);
-//
-//				}
-//			}
-//		});
+
 		setup();//准备播放
 		play();//开始播放
 		
@@ -309,16 +184,6 @@ public class PlayActivity extends Activity implements MediaPlayer.OnCompletionLi
 					playtime.setText(toTime(mp.getCurrentPosition()));//初始化播放时间
 					durationTime.setText(toTime(mp.getDuration()));//设置歌曲时间
 					mp.seekTo(currentPosition);//初始化MediaPlayer播放位置
-					/*获得音乐最大音量*/
-					mAudioManager = (AudioManager) PlayActivity.this.getSystemService(PlayActivity.this.AUDIO_SERVICE);
-					int maxSound = mAudioManager.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
-					
-					/*获得当前音乐音量*/
-					int currentSound = mAudioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
-					
-//					soundBar.setMax(maxSound);
-//					soundBar.setProgress(currentSound);
-
 				}
 			});
 		} catch (Exception e) {
@@ -560,10 +425,9 @@ public class PlayActivity extends Activity implements MediaPlayer.OnCompletionLi
 						MediaStore.Audio.Media.DATA}, "_id=?",
 				new String[] { _ids[position] + "" }, null);
 		myCur.moveToFirst();
-//
+
 		String name = myCur.getString(5).substring(0,
 				myCur.getString(5).lastIndexOf("."));
-		System.out.println(name);
 		read( name + ".lrc");
 	}
 
